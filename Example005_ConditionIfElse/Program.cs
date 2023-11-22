316886088df460bad22dbf56184fe09f81ac0571
Denis Lodygin <Alirka94@mail.ru>
2023-11-22 22:14:45 +0500

    Добавили приветствие Марины

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.Tolower() == "Марина")
+{
+    Console.WriteLine("Ура Это же Марина, я тебя люблю");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
